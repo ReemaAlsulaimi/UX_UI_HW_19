b61eec0c45245237aec851f765ebf81289510810
ReemaAlsulaimi <115298523+ReemaAlsulaimi@users.noreply.github.com>
2023-05-04 21:00:19 -0400

    Delete UX_UI_HW_19 directory

--- UX_UI_HW_19/js/index.js
+++
@@ -1,8 +0,0 @@
-console.log("your index.js file is loaded correctly!");
-
-$( "AGRICULTURE" ).on( "click", function() {
-  $( this ).slideUp();
-} );
-$( "TheWayHome" ).on( "click", function() {
-  $( this ).slideUp();
-} );
