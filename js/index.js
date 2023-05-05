7c02906d6c3bbf8c2bfcac5044b303fe32a26fde
ReemaAlsulaimi <115298523+ReemaAlsulaimi@users.noreply.github.com>
2023-05-04 21:01:45 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.log("your index.js file is loaded correctly!");
+
+$( "AGRICULTURE" ).on( "click", function() {
+  $( this ).slideUp();
+} );
+$( "TheWayHome" ).on( "click", function() {
+  $( this ).slideUp();
+} );
